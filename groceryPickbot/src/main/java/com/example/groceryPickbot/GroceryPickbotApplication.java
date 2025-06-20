950f231a9d6204916bac4b28a46b2564d8b55e5b
Kostadin Harizanov <kokiharizanov@gmail.com>
2025-06-20 17:18:53 +0300

    Initial project structure for grocery_pickbot

---
+++ groceryPickbot/src/main/java/com/example/groceryPickbot/GroceryPickbotApplication.java
@@ -0,0 +1,13 @@
+package com.example.groceryPickbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryPickbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryPickbotApplication.class, args);
+	}
+
+}
---
+++ groceryPickbot/src/test/java/com/example/groceryPickbot/GroceryPickbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.groceryPickbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryPickbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
